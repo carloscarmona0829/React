357877a3690fbbe26c9df48b0d64fda3aa2578dd
Carlos Alberto Carmona Alvarez <carlos.carmona@countryclub.com.co>
2022-09-12 17:33:30 -0500

    Componente CrudApi sin terminar

--- 02-react-ejercicios/src/App.js
+++ 02-react-ejercicios/src/App.js
@@ -1,10 +1,14 @@
+import CrudApi from "./components/CrudApi";
 import CrudAPP from "./components/CrudAPP";
 
 function App() {
   return (
     <>
-      <h1>Ejercicios con React</h1>   
-      <CrudAPP />  
+      <h1>Ejercicios con React</h1>
+      <hr />
+      <CrudApi />
+      <hr />
+      <CrudAPP />
     </>
   );
 }
