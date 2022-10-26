5b9a064509165196a5a69a73b1caeae63e6b07d4
Carlos Alberto Carmona Alvarez <carlos.carmona@countryclub.com.co>
2022-10-26 17:20:51 -0500

    Reducers Carrito Comras 1/5 terminado

--- 06-react-reducers/src/App.js
+++ 06-react-reducers/src/App.js
@@ -1,15 +1,18 @@
-import './App.css';
-import Contador from './components/Contador';
-import ContadorMejorado from './components/ContadorMejorado';
+import "./App.css";
+import Contador from "./components/Contador";
+import ContadorMejorado from "./components/ContadorMejorado";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
     <div>
       <h1>UseReducer</h1>
       <hr />
+      <ShoppingCart />
+      <hr />
       <ContadorMejorado />
       <hr />
-      <Contador />      
+      <Contador />
     </div>
   );
 }
---
+++ 06-react-reducers/src/components/ShoppingCart.js
@@ -0,0 +1,13 @@
+const ShoppingCart = () => {
+  return (
+    <div>
+      <h2>Carito de Compras</h2>
+      <h3>Productos</h3>
+      <article className="box"></article>
+      <h3>Carito de Compras</h3>
+      <article className="box"></article>
+    </div>
+  );
+};
+
+export default ShoppingCart;
