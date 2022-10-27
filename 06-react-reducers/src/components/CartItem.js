ebfb554f758437bc22c630a629869ad5f3996dd6
Carlos Alberto Carmona Alvarez <carlos.carmona@countryclub.com.co>
2022-10-27 16:54:10 -0500

    Reducers Carrito Comras 2/5 hasta 5/5 terminados

---
+++ 06-react-reducers/src/components/CartItem.js
@@ -0,0 +1,17 @@
+const CartItem = ({ data, delFromCart }) => {
+  let { id, name, price, quantity } = data;
+  return (
+    <div style={{ border: "thin solid gray", padding: "1rem" }}>
+      <h4>{quantity}</h4>
+      <h4>{name}</h4>
+      <h5>${price}.00</h5>
+      <button onClick={() => delFromCart(id)}>Eliminar Uno</button>
+      <br />
+      <button onClick={() => delFromCart(id, true)}>Eliminar Todos</button>
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default CartItem;
