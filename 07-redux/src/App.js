cade8e35fb28e6f0698dc0ef2819bd1627a4667f
carloscarmona0829 <carloscarmona0829@gmail.com>
2022-10-29 11:18:44 -0500

    Introducción Redux Teminada

---
+++ 07-redux/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TeoriaRedux from './components/TeoriaRedux';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+        <h1>Redux</h1>  
+        <TeoriaRedux />
+      </div>
+  );
+}
+
+export default App;
